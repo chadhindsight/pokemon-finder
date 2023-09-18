ae3270ee467f707a5347d4f0f4ff2273d20cf962
chad hinds <chadrickj8@gmail.com>
2023-09-18 12:39:58 -0400

    whatevz

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
 // <ThemeProvder cooper="very cool" defaultState="dark"/>
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
