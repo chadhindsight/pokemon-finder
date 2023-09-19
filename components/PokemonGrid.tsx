b5c92d21ef6f6f8724d58aac3c29b2c70c764ffd
chad hinds <chadrickj8@gmail.com>
2023-09-19 12:44:52 -0400

    Remove comment

--- components/PokemonGrid.tsx
+++ components/PokemonGrid.tsx
@@ -4,8 +4,6 @@ import { useState } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 
-// <PokemonGrid pokemonList={data}/>
-
 interface PokemonGridProps {
     pokemonList: any
 }
