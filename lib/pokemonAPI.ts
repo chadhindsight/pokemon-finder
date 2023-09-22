226ee3bdbd6b7fda3910f909d19642470f6e309f
chad hinds <chadrickj8@gmail.com>
2023-09-22 15:31:47 -0400

    Make a singlePokeData interface

--- lib/pokemonAPI.ts
+++ lib/pokemonAPI.ts
@@ -2,18 +2,46 @@
 
 const POKEMON_API = "https://pokeapi.co/api/v2/";
 
-interface responseData {
+interface listOfData {
     count: number
     next: string
     previous: boolean
     results: Array<{ name: string, url: string }>
 }
+
+interface singlePokeData {
+    abilities: []
+    base_experience: number
+    forms: []
+    game_indices: []
+    height: number
+    held_items: []
+    id: number
+    is_default: boolean
+    location_area_encounters: string
+    moves: []
+    name: string
+    order: number
+    past_types: []
+    species: {}
+    sprites: {}
+    stats: []
+    types: []
+    weight: number
+}
+
 // get the original 151 Pokemon
 export async function getPokemonList() {
     const response = await fetch(`${POKEMON_API}pokemon?limit=151&offset=0`)
 
-    const data: responseData = await response.json();
+    const data: listOfData = await response.json();
     return data.results;
 }
 
 // get content for a specific Pokemon
+export async function getPokemon(name: string) {
+    const response = await fetch(`${POKEMON_API}pokemon/${name}`)
+
+    const data: singlePokeData = await response.json();
+    return data
+}
